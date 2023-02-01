780ff2e8311ea835a54a4371b50bfdcc4e975b90
senior_Ori <ori8834@gmail.com>
2023-02-01 15:38:55 +0200

    BLINK_LED + DELAY

--- main/smart-mails.c
+++ main/smart-mails.c
@@ -5,13 +5,23 @@
 
 #include "esp_log.h"
 
+#include "driver/gpio.h"
+#define BLINK_LED 2
+
 void app_main(void)
 {
     char *ourTaskName = pcTaskGetName(NULL);
 
     ESP_LOGI(ourTaskName, "Hello, starting up!\n");
+
+    gpio_reset_pin(BLINK_LED);
+    gpio_set_direction(BLINK_LED, GPIO_MODE_OUTPUT);
+
     while (1)
     {
-        vTaskDelay(1000);
+        gpio_set_level(BLINK_LED, 0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        gpio_set_level(BLINK_LED, 1);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
