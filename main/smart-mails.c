da74c7b6b9e5fe9b65a320dc1759dbeaf73bee2f
senior_Ori <ori8834@gmail.com>
2023-02-14 00:32:11 +0200

    http error handler

--- main/smart-mails.c
+++ main/smart-mails.c
@@ -187,6 +187,7 @@ esp_err_t client_event_post_handler(esp_http_client_event_handle_t evt)
         break;
 
     default:
+        printf("A problem has been found!\n");
         break;
     }
     return ESP_OK;
